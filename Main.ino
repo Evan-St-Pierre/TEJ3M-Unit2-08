14f2c9d62da752ddeebf86567c9a1604da05899f
Evan-St-Pierre <144252383+Evan-St-Pierre@users.noreply.github.com>
2023-11-13 10:58:39 -0500

    update final

--- Main.ino
+++ Main.ino
@@ -10,16 +10,16 @@ Servo mainServo;
 
 void setup() {
   Serial.begin(9600);
-  mainServo.attach(8);
+  mainServo.attach(8); // Pin number
   mainServo.write(0);
   delay(1000);
  
 }
 void loop() {
   
-  const double diffValue = 1023 / 180;                 
+  const double diffValue = 1023 / 180; // Max rotation value of potentiometer / Max rotation of servo degree             
   int sensorValue = analogRead(A0);
-  int angle = sensorValue / diffValue;
+  int angle = sensorValue / diffValue; // Lets servo know what degree to rotate to based on potentiometer position.
  
   Serial.print(sensorValue);
   Serial.print(":");
